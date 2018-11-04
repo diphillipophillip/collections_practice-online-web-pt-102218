edef22f910ca23ccb2afc8071cef7370d64b7a96
Phillip DiPhillipo <diphillipophillip@gmail.com>
2018-11-04 21:18:10 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,25 @@
+def sort_array_asc(array)
+  array.sort 
+end 
 
+def sort_array_desc(array)
+  array_new = array.sort  
+      array_new.reverse
+end 
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a.length <=> b.length 
+end
+end
+
+def swap_elements(array)
+  
+end 
+
+
+
+
+
+
+end
